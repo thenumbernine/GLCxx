b6c83617fca7cebd8dc9cbb20fa3d19133c3486a
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-09-08 16:45:53 +0300

    updating

--- Include.mk
+++ Include.mk
@@ -1,5 +1,5 @@
 SHADER_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE+=$(SHADER_PATH)include
-DYNAMIC_LIBS+=$(SHADER_PATH)dist/$(PLATFORM)/$(BUILD)/libShader.dylib
+DYNAMIC_LIBS+=$(SHADER_PATH)dist/$(PLATFORM)/$(BUILD)/libShader$(LIB_SUFFIX)
 
