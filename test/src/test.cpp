c2ff14277f39928e7d0372e94e5770684eed1688
Christopher Moore <christopher.e.moore@gmail.com>
2019-11-13 00:32:14 +0600

    fixed to work with new GLApp

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,17 +1,25 @@
+#include "Shader/Program.h"
 #include "GLApp/gl.h"
 #include "GLApp/GLApp.h"
+#include "GLApp/ViewBehavior.h"
 #include "Common/File.h"
-#include "Shader/Program.h"
 
-struct Test : public GLApp::GLApp {
-	float angle;
+#include <chrono>
+
+struct Test : public ::GLApp::ViewBehavior<::GLApp::GLApp> {
+	using Super = ::GLApp::ViewBehavior<::GLApp::GLApp>;
+	
+	using Clock = std::chrono::high_resolution_clock;
+	std::chrono::time_point<Clock> lastTime = Clock::now();
+	
 	std::shared_ptr<Shader::Program> shaderProgram;
 
-	Test() : GLApp(), angle(0.) {}
+	float angle = 0;
 	
 	virtual void init() {
-		GLApp::init();
+		Super::init();
 		glClearColor(.5, .75, .75, 1.);
+		viewFrustum->dist = 3.;
 	
 		std::string shaderCode = Common::File::read("test.shader");
 		std::vector<Shader::Shader> shaders = {
@@ -21,23 +29,14 @@ struct Test : public GLApp::GLApp {
 		shaderProgram = std::make_shared<Shader::Program>(shaders);
 	}
 	
-	virtual void resize(int width, int height) {
-		GLApp::resize(width, height);
-		float aspectRatio = (float)width / (float)height;
-		float zNear = .1;
-		float zFar = 100;
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glFrustum(-aspectRatio * zNear, aspectRatio * zNear, -zNear, zNear, zNear, zFar);
-	}
-	
 	virtual void update() {
-		GLApp::update();
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glTranslatef(0, 0, -2);
-		glRotatef(angle,0,1,0);
+		std::chrono::time_point<Clock> thisTime = Clock::now();
+		float deltaTime = 1e-9 * (double)std::chrono::duration_cast<std::chrono::nanoseconds>(thisTime - lastTime).count();
+		lastTime = thisTime;
 		
+		Super::update();
+		glMatrixMode(GL_MODELVIEW);
+		glRotatef(angle,0,1,0);
 		shaderProgram->use();
 		glBegin(GL_TRIANGLES);
 		glColor3f(1,0,0);
@@ -49,7 +48,7 @@ struct Test : public GLApp::GLApp {
 		glEnd();
 		shaderProgram->done();
 		
-		angle += .01;
+		angle += deltaTime * 360;	//1 revolution per second
 	}
 };
 
